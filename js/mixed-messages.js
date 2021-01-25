6ecc85f1d12265224300f3aa4947e568c8c173c4
Sour Apple Rings <76459670+sourapplerings@users.noreply.github.com>
2021-01-24 20:02:26 -0800

    Added interactivity

--- js/mixed-messages.js
+++ js/mixed-messages.js
@@ -1,29 +1,34 @@
+let element = document.getElementById('button');
+let message = document.getElementById('message');
+
 const random = Math.floor((Math.random()*10)+1);
 
 const getMessage = random => {
     if (random === 1) {
-        return 'Your future looks bleak.'
+        message.innerHTML = 'Your future looks bleak.'
     } else if (random === 2) {
-        return 'Try again.'
+        message.innerHTML = 'Try again.'
     } else if (random === 3) {
-        return 'The answer is uncertain. Tread carefully.'
+        message.innerHTML = 'The answer is uncertain. Tread carefully.'
     } else if (random === 4) {
-        return 'The time is now.'
+        message.innerHTML = 'The time is now.'
     } else if (random === 5) {
-        return 'There is no time like the present.'
+        message.innerHTML = 'There is no time like the present.'
     } else if (random === 6) {
-        return 'Look inward for the answer.'
+        message.innerHTML = 'Look inward for the answer.'
     } else if (random === 7) {
-        return 'Seek advice from a close friend.'
+        message.innerHTML = 'Seek advice from a close friend.'
     } else if (random === 8) {
-        return 'That is a clear no.'
+        message.innerHTML = 'That is a clear no.'
     } else if (random === 9) {
-        return 'Happiness does not require you to do so.'
+        message.innerHTML = 'Happiness does not require you to do so.'
     } else if (random === 10) {
-        return 'Pray.'
+        message.innerHTML = 'Pray.'
     } else {
-        return 'Jesus is the answer.'
+        message.innerHTML = 'Jesus is the answer.'
     }
-}
+};
 
-document.getElementById("button").onclick = getMessage;
+element.onclick = getMessage
+
+
