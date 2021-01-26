d58d9080fe2ec45652cbfe981096cff057475ff6
Sour Apple Rings <76459670+sourapplerings@users.noreply.github.com>
2021-01-25 20:16:36 -0800

    Added CSS and, 8-ball, and event handler

--- js/mixed-messages.js
+++ js/mixed-messages.js
@@ -1,34 +1,18 @@
-let element = document.getElementById('button');
+let messages = ['Outlook not so good.', 'Try again.', 'The answer is uncertain.', 'Tread carefully.',
+    'The time is now.', 'There is no time like the present.', 'Look inward for the answer.',
+    'Seek advice from a close friend.', 'That is a clear no.', 'Happiness does not require you to do so.',
+    'Pray.', 'Jesus is the answer.', 'You must wait longer.']
+
+let button = document.getElementById('button');
 let message = document.getElementById('message');
 
-const random = Math.floor((Math.random()*10)+1);
+function messageSelector() {
+    let random = Math.floor(Math.random() * messages.length);
+    return messages[random];
+}
 
-const getMessage = random => {
-    if (random === 1) {
-        message.innerHTML = 'Your future looks bleak.'
-    } else if (random === 2) {
-        message.innerHTML = 'Try again.'
-    } else if (random === 3) {
-        message.innerHTML = 'The answer is uncertain. Tread carefully.'
-    } else if (random === 4) {
-        message.innerHTML = 'The time is now.'
-    } else if (random === 5) {
-        message.innerHTML = 'There is no time like the present.'
-    } else if (random === 6) {
-        message.innerHTML = 'Look inward for the answer.'
-    } else if (random === 7) {
-        message.innerHTML = 'Seek advice from a close friend.'
-    } else if (random === 8) {
-        message.innerHTML = 'That is a clear no.'
-    } else if (random === 9) {
-        message.innerHTML = 'Happiness does not require you to do so.'
-    } else if (random === 10) {
-        message.innerHTML = 'Pray.'
-    } else {
-        message.innerHTML = 'Jesus is the answer.'
-    }
-};
+function getMessage() {
+    message.innerHTML = messageSelector();
+}
 
-element.onclick = getMessage
-
-
+button.addEventListener('click', getMessage);
